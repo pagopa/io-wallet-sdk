603e886805e5d596df247860a5af88086e424cdf
Manuel Rafeli <manuel.rafeli@pagopa.it>
2025-09-04 15:51:27 +0200

    feat(oid-federation): [WLEO-513] Add oid-federation metadata (#2)

---
+++ packages/oid-federation/src/entityConfiguration/itWalletEntityConfigurationClaims.ts
@@ -0,0 +1,16 @@
+import type { z } from "zod";
+import { itWalletEntityStatementClaimsSchema } from "../entityStatement/itWalletEntityStatementClaims";
+
+export const itWalletEntityConfigurationClaimsSchema =
+  itWalletEntityStatementClaimsSchema.refine((data) => data.iss === data.sub, {
+    message: "iss and sub must be equal",
+    path: ["iss", "sub"],
+  });
+
+export type ItWalletEntityConfigurationClaimsOptions = z.input<
+  typeof itWalletEntityConfigurationClaimsSchema
+>;
+
+export type ItWalletEntityConfigurationClaims = z.output<
+  typeof itWalletEntityConfigurationClaimsSchema
+>;
---
+++ packages/oid-federation/src/metadata/entity/itWalletFederationEntity.ts
@@ -0,0 +1,19 @@
+import { federationEntityMetadata } from "@openid-federation/core";
+import { z } from "zod";
+/**
+ *
+ * {@link https://italia.github.io/eid-wallet-it-docs/releases/1.1.0/en/trust.html#metadata-of-federation-entity-leaves}
+ *
+ */
+export const itWalletFederationEntityMetadata = federationEntityMetadata.schema
+  .extend({
+    tos_uri: z.string().url().optional(),
+  })
+  .passthrough();
+
+export type ItWalletFederationEntityMetadata = z.input<
+  typeof itWalletFederationEntityMetadata
+>;
+
+export const itWalletFederationEntityIdentifier =
+  federationEntityMetadata.identifier;
