c62ed52b33c9467aa54ca9c12fe83de82273c924
LazyAfternoons <lazyafternoons@outlook.it>
2025-09-23 16:07:56 +0200

    chore: remove unwanted changes

--- packages/oauth2/src/authorization-request/z-authorization-request.ts
+++ packages/oauth2/src/authorization-request/z-authorization-request.ts
@@ -1,4 +1,4 @@
-import z from "zod";
+import z from 'zod'
 
 export const zAuthorizationRequest = z
   .object({
@@ -9,40 +9,35 @@ export const zAuthorizationRequest = z
     code_challenge: z.string(),
     code_challenge_method: z.string(),
     scope: z.string(),
-    authorization_details: z.array(
-      z.object({
-        type: z.literal("openid_credential"),
-        credential_configuration_id: z.string(),
-      })
-    ),
+    authorization_details: z.array(z.object({
+      type: z.literal("openid_credential"),
+      credential_configuration_id: z.string()
+    })),
     redirect_uri: z.string().url().optional(),
     issuer_state: z.optional(z.string()),
+
   })
-  .passthrough();
-export type AuthorizationRequest = z.infer<typeof zAuthorizationRequest>;
+  .passthrough()
+export type AuthorizationRequest = z.infer<typeof zAuthorizationRequest>
 
 export const zPushedAuthorizationRequestSigned = z
   .object({
-    /*
-     * It MUST be a signed JWT. The private key corresponding to the public one in the cnf parameter inside the Wallet Attestation MUST be used for signing the Request Object.
-     */
+    /* 
+    * It MUST be a signed JWT. The private key corresponding to the public one in the cnf parameter inside the Wallet Attestation MUST be used for signing the Request Object.
+    */
     request: z.string(),
-    /*
-     * MUST be set to the thumbprint of the jwk value in the cnf parameter inside the Wallet Attestation.
-     */
+    /* 
+    * MUST be set to the thumbprint of the jwk value in the cnf parameter inside the Wallet Attestation.
+    */
     client_id: z.string(),
   })
-  .passthrough();
-export type PushedAuthorizationRequestSigned = z.infer<
-  typeof zPushedAuthorizationRequestSigned
->;
+  .passthrough()
+export type PushedAuthorizationRequestSigned = z.infer<typeof zPushedAuthorizationRequestSigned>
 
 export const zPushedAuthorizationResponse = z
   .object({
     request_uri: z.string(),
     expires_in: z.number().int(),
   })
-  .passthrough();
-export type PushedAuthorizationResponse = z.infer<
-  typeof zPushedAuthorizationResponse
->;
+  .passthrough()
+export type PushedAuthorizationResponse = z.infer<typeof zPushedAuthorizationResponse>
