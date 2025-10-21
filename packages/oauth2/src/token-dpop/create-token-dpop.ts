baedb74216af2310b91992446a29382c7961d650
Manuel Rafeli <manuel.rafeli@pagopa.it>
2025-10-21 17:23:04 +0200

    feat: refactor createTokenDPoP adding ath

--- packages/oauth2/src/token-dpop/create-token-dpop.ts
+++ packages/oauth2/src/token-dpop/create-token-dpop.ts
@@ -1,45 +1,71 @@
-import { CallbackContext, HttpMethod, JwtSigner } from "@openid4vc/oauth2";
+import {
+  CallbackContext,
+  HashAlgorithm,
+  HttpMethod,
+  JwtSignerJwk,
+} from "@openid4vc/oauth2";
+import {
+  dateToSeconds,
+  decodeUtf8String,
+  encodeToBase64Url,
+  parseWithErrorHandling,
+} from "@openid4vc/utils";
 import { Base64 } from "js-base64";
 
 import { CreateTokenDPoPError } from "../errors";
+import {
+  DpopJwtHeader,
+  DpopJwtPayload,
+  zDpopJwtHeader,
+  zDpopJwtPayload,
+} from "./z-dpop";
 
 /**
  * Options for Token Request DPoP generation
  */
 export interface CreateTokenDPoPOptions {
   /**
+   * The access token to which the dpop jwt should be bound. Required
+   * when the dpop will be sent along with an access token.
+   *
+   * If provided, the `hashCallback` parameter also needs to be provided
+   */
+  accessToken?: string;
+
+  /**
    * Object containing callbacks for DPoP generation and signature
    */
   callbacks: Partial<Pick<CallbackContext, "generateRandom">> &
-    Pick<CallbackContext, "signJwt">;
+    Pick<CallbackContext, "hash" | "signJwt">;
 
   /**
-   * Customizable headers for DPoP signing.
-   * As per technical specifications, the key typ will be set to 'dpop+jwt',
-   * overriding any custom value passed. In case the alg and jwk properties
-   * will not be set, the responsibility of doing so is left to the signJwt
-   * callback, which may as well override such keys if passed
+   * Creation time of the JWT. If not provided the current date will be used
    */
-  header: { alg: string } & Record<string, unknown>;
+  issuedAt?: Date;
 
   /**
-   * Customizable payload for DPoP signing.
-   * Any field might be overridden by the signJwt callback
+   * jti claim for the DPoP JWT. If not provided, a random one will be generated
+   * if a generateRandom callback is provided
    */
-  payload: {
-    htm: HttpMethod;
-    htu: string;
-    jti?: string;
-  } & Record<string, unknown>;
+  jti?: string;
 
   /**
-   * Jwt Signer corresponding to the DPoP's Crypto Context
+   * The signer of the dpop jwt. Only jwk signer allowed.
    */
-  signer: JwtSigner;
+  signer: JwtSignerJwk;
+
+  /**
+   * The request for which to create the dpop jwt
+   */
+  tokenRequest: {
+    method: HttpMethod;
+    url: string;
+  };
 }
 
 /**
  * Creates a signed Token DPoP with the given cryptographic material and data.
+ * It is used to create DPoP proofs for token requests and credential requests.
  * @param options {@link CreateTokenDPoPOptions}
  * @returns A Promise that resolves with an object containing the signed DPoP JWT and
  *          its corresponding public JWK
@@ -47,8 +73,18 @@ export interface CreateTokenDPoPOptions {
  *         callback have been provided or the signJwt callback throws
  */
 export async function createTokenDPoP(options: CreateTokenDPoPOptions) {
+  // Calculate access token hash
+  const ath = options.accessToken
+    ? encodeToBase64Url(
+        await options.callbacks.hash(
+          decodeUtf8String(options.accessToken),
+          HashAlgorithm.Sha256,
+        ),
+      )
+    : undefined;
+
   const jti =
-    options.payload.jti ??
+    options.jti ??
     (options.callbacks.generateRandom
       ? Base64.fromUint8Array(await options.callbacks.generateRandom(32), true)
       : undefined);
@@ -58,16 +94,24 @@ export async function createTokenDPoP(options: CreateTokenDPoPOptions) {
       "Error: neither a default jti nor a generateRandom callback have been provided",
     );
   }
+
+  const header = parseWithErrorHandling(zDpopJwtHeader, {
+    alg: options.signer.alg,
+    jwk: options.signer.publicJwk,
+    typ: "dpop+jwt",
+  } satisfies DpopJwtHeader);
   try {
+    const payload = parseWithErrorHandling(zDpopJwtPayload, {
+      ath,
+      htm: options.tokenRequest.method,
+      htu: htuFromRequestUrl(options.tokenRequest.url),
+      iat: dateToSeconds(options.issuedAt),
+      jti,
+    } satisfies DpopJwtPayload);
+
     return options.callbacks.signJwt(options.signer, {
-      header: {
-        ...options.header,
-        typ: "dpop+jwt",
-      },
-      payload: {
-        ...options.payload,
-        jti,
-      },
+      header,
+      payload,
     });
   } catch (error) {
     throw new CreateTokenDPoPError(
@@ -75,3 +119,11 @@ export async function createTokenDPoP(options: CreateTokenDPoPOptions) {
     );
   }
 }
+
+const htuFromRequestUrl = (requestUrl: string) => {
+  const htu = new URL(requestUrl);
+  htu.search = "";
+  htu.hash = "";
+
+  return htu.toString();
+};
