1306958da871a3b56495cf6a88be005fc73234a2
sebastianopriscan <sebastianroberto.opriscan@gmail.com>
2025-10-28 15:26:31 +0100

    chore: [WLEO-543] Ran linter

--- packages/oid4vp/src/authorization-response/index.ts
+++ packages/oid4vp/src/authorization-response/index.ts
@@ -1,3 +1,3 @@
+export * from "./create-authorization-response";
 export * from "./fetch-authorization-response";
 export * from "./z-authorization-response";
-export * from "./create-authorization-response";
