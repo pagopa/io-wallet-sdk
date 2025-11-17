f860ead635d5831133f9e2534898dcbaeba9f6d4
Manuel Rafeli <manuel.rafeli@pagopa.it>
2025-11-17 10:01:51 +0100

    fix: remove iss must be equal sub

--- packages/oid-federation/src/entityConfiguration/itWalletEntityConfigurationClaims.ts
+++ packages/oid-federation/src/entityConfiguration/itWalletEntityConfigurationClaims.ts
@@ -3,10 +3,7 @@ import type { z } from "zod";
 import { itWalletEntityStatementClaimsSchema } from "../entityStatement/itWalletEntityStatementClaims";
 
 export const itWalletEntityConfigurationClaimsSchema =
-  itWalletEntityStatementClaimsSchema.refine((data) => data.iss === data.sub, {
-    message: "iss and sub must be equal",
-    path: ["iss", "sub"],
-  });
+  itWalletEntityStatementClaimsSchema;
 
 export type ItWalletEntityConfigurationClaimsOptions = z.input<
   typeof itWalletEntityConfigurationClaimsSchema
