ee7f610cddee2d41652e433a560b5551b4248e1c
Flavio Del Grosso <flavio.delgrosso@nttdata.com>
2026-02-04 15:05:30 +0100

    fix: export verifyAuthorizationCodeTokenRequest function and all its exported types

--- packages/oauth2/src/access-token/index.ts
+++ packages/oauth2/src/access-token/index.ts
@@ -1,5 +1,6 @@
 export * from "./create-token-request";
 export * from "./fetch-token-response";
 export * from "./parse-token-request";
+export * from "./verify-authorization-code-token-request";
 export * from "./z-grant-type";
 export * from "./z-token";
