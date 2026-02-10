7ad26c196ccecef71d9258c135c42d34c57f55e9
Manuel Rafeli <manuel.rafeli@pagopa.it>
2026-02-10 10:38:12 +0100

    feat(oid-federation): [WLEO-802] Add v1.3 metadata validation for OAuth authorization server (#77)
    #### List of Changes
    
    Introduce validation for the v1.3 metadata of the OAuth authorization
    server, including new schemas and properties as per the updated
    specifications.
    
    #### Motivation and Context
    
    This change is required to ensure compliance with the latest v1.3
    standards for OAuth authorization servers, enhancing the validation
    process for metadata.
    
    #### How Has This Been Tested?
    
    Changes were tested by validating the new metadata schemas against
    expected inputs and ensuring that all required fields are correctly
    enforced. Testing was conducted in a controlled environment to verify
    compliance with the updated specifications.

--- packages/oid-federation/src/metadata/entity/v1.3/itWalletAuthorizationServer.ts
+++ packages/oid-federation/src/metadata/entity/v1.3/itWalletAuthorizationServer.ts
@@ -1,5 +1,68 @@
-export {
-  itWalletAuthorizationServerIdentifier,
-  itWalletAuthorizationServerMetadata,
-} from "../v1.0/itWalletAuthorizationServer";
-export type { ItWalletAuthorizationServerMetadata } from "../v1.0/itWalletAuthorizationServer";
+import { z } from "zod";
+
+import { jsonWebKeySetSchema } from "../../../jwk";
+
+/**
+ *
+ * {@link https://italia.github.io/eid-wallet-it-docs/releases/1.3.3/en/credential-issuer-solution.html#metadata-for-oauth-authorization-server}
+ *
+ */
+export const itWalletAuthorizationServerMetadata = z
+  .object({
+    acr_values_supported: z.array(
+      z.union([
+        z.literal("https://trust-anchor.eid-wallet.example.it/loa/low"),
+        z.literal("https://trust-anchor.eid-wallet.example.it/loa/substantial"),
+        z.literal("https://trust-anchor.eid-wallet.example.it/loa/high"),
+      ]),
+    ),
+    authorization_endpoint: z.string().url(),
+    authorization_signing_alg_values_supported: z.array(z.string()),
+    client_attestation_pop_signing_alg_values_supported: z
+      .array(z.string())
+      .min(1),
+    client_attestation_signing_alg_values_supported: z.array(z.string()).min(1),
+    client_registration_types_supported: z.array(
+      z.union([z.literal("automatic"), z.literal("explicit")]),
+    ),
+    code_challenge_methods_supported: z
+      .array(z.string())
+      .refine((arr) => arr.includes("S256"), {
+        message:
+          "The code_challenge_methods_supported array MUST include 'S256'.",
+      }),
+    dpop_signing_alg_values_supported: z.array(z.string()).min(1),
+    grant_types_supported: z
+      .array(z.string())
+      .refine((arr) => arr.includes("authorization_code"), {
+        message:
+          "The grant_types_supported array MUST include 'authorization_code'.",
+      }),
+    issuer: z.string().url(),
+    jwks: jsonWebKeySetSchema,
+    pushed_authorization_request_endpoint: z.string().url(),
+    request_object_signing_alg_values_supported: z.array(z.string()),
+    require_signed_request_object: z.literal(true),
+    response_types_supported: z
+      .array(z.string())
+      .refine((arr) => arr.includes("code"), {
+        message: "The response_types_supported array MUST include 'code'.",
+      }),
+    scopes_supported: z.array(z.string()),
+    token_endpoint: z.string().url(),
+    token_endpoint_auth_methods_supported: z
+      .array(z.string())
+      .refine((arr) => arr.includes("attest_jwt_client_auth"), {
+        message:
+          "The token_endpoint_auth_methods_supported array MUST include 'attest_jwt_client_auth'.",
+      }),
+    token_endpoint_auth_signing_alg_values_supported: z.array(z.string()),
+  })
+  .passthrough();
+
+export type ItWalletAuthorizationServerMetadata = z.input<
+  typeof itWalletAuthorizationServerMetadata
+>;
+
+export const itWalletAuthorizationServerIdentifier =
+  "oauth_authorization_server";
