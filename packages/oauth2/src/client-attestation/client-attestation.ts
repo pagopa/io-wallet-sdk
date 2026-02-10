84a5be5199833327bc586db438641a0fc9828ca8
Flavio Del Grosso <flavio.delgrosso@nttdata.com>
2026-02-10 16:35:58 +0100

    feat: add createClientAttestationJwt utility function

--- packages/oauth2/src/client-attestation/client-attestation.ts
+++ packages/oauth2/src/client-attestation/client-attestation.ts
@@ -1,13 +1,21 @@
 import {
   CallbackContext,
+  JwtSigner,
   decodeJwt,
+  jwtHeaderFromJwtSigner,
   jwtSignerFromJwt,
   verifyJwt,
   zCompactJwt,
 } from "@openid4vc/oauth2";
-import { FetchHeaders } from "@pagopa/io-wallet-utils";
+import {
+  FetchHeaders,
+  dateToSeconds,
+  parseWithErrorHandling,
+} from "@pagopa/io-wallet-utils";
 
 import {
+  ClientAttestationJwtHeader,
+  ClientAttestationJwtPayload,
   oauthClientAttestationHeader,
   oauthClientAttestationPopHeader,
   zClientAttestationJwtHeader,
@@ -101,4 +109,84 @@ export function extractClientAttestationJwtsFromHeaders(headers: FetchHeaders):
     clientAttestationPopHeader,
     valid: true,
   } as const;
+}
+
+export interface CreateClientAttestationJwtOptions {
+  /**
+   * Additional payload to include in the client attestation jwt payload. Will be applied after
+   * any default claims that are included, so add claims with caution.
+   */
+  additionalPayload?: Record<string, unknown>;
+
+  /**
+   * It expresses the strength of the authentication mechanism backing the Wallet instance when interacting with a Relying Party.
+   */
+  authenticatorAssuranceLevel: ClientAttestationJwtPayload["aal"];
+
+  /**
+   * Callback used for client attestation
+   */
+  callbacks: Pick<CallbackContext, "signJwt">;
+
+  /**
+   * The client id of the client instance.
+   */
+  clientId: string;
+
+  /**
+   * The confirmation payload for the client, attesting the `jwk`, `key_type` and `user_authentication`
+   */
+  confirmation: ClientAttestationJwtPayload["cnf"];
+
+  /**
+   * Expiration time of the JWT.
+   */
+  expiresAt: Date;
+
+  /**
+   * Creation time of the JWT. If not provided the current date will be used
+   */
+  issuedAt?: Date;
+
+  /**
+   * Issuer of the client attestation, usually identifier of the client backend
+   */
+  issuer: string;
+
+  /**
+   * The signer of the client attestation jwt.
+   */
+  signer: JwtSigner;
+
+  /**
+   * Chain of trust for the client attestation jwt, containing the thumbprints of the jwk values in the cnf parameter inside Wallet Attestation, starting from the client and up to the root of trust.
+   */
+  trustChain: ClientAttestationJwtHeader["trust_chain"];
+}
+
+export async function createClientAttestationJwt(
+  options: CreateClientAttestationJwtOptions,
+) {
+  const header = parseWithErrorHandling(zClientAttestationJwtHeader, {
+    ...jwtHeaderFromJwtSigner(options.signer),
+    trust_chain: options.trustChain,
+    typ: "oauth-client-attestation+jwt",
+  } satisfies ClientAttestationJwtHeader);
+
+  const payload = parseWithErrorHandling(zClientAttestationJwtPayload, {
+    aal: options.authenticatorAssuranceLevel,
+    cnf: options.confirmation,
+    exp: dateToSeconds(options.expiresAt),
+    iat: dateToSeconds(options.issuedAt),
+    iss: options.issuer,
+    sub: options.clientId,
+    ...options.additionalPayload,
+  } satisfies ClientAttestationJwtPayload);
+
+  const { jwt } = await options.callbacks.signJwt(options.signer, {
+    header,
+    payload,
+  });
+
+  return jwt;
 }
