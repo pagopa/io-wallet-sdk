19356276c8f80a7346daf50d5fe8c04f5c5ed500
Flavio Del Grosso <flavio.delgrosso@nttdata.com>
2026-03-04 18:45:45 +0100

    fix: update error message in vptoken schema

--- packages/oid4vp/src/vp-token/z-vp-token.ts
+++ packages/oid4vp/src/vp-token/z-vp-token.ts
@@ -5,7 +5,7 @@ export const zVpToken = z.record(
   z.string().or(z.array(z.string()).nonempty()),
   {
     message:
-      "vp_token must be an object where each key is a string and each value is a non-empty array of strings.",
+      "vp_token must be an object where each key is a string and each value is a non-empty array of strings (v1.3) or a string (v1.0)",
   },
 );
 
