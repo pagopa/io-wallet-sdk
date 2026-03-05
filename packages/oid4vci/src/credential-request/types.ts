76b8118bb2ca3c61ac118d73956891f881e7d87c
Gianluca Spada <gianluca.spada@spindox.it>
2026-03-05 10:25:57 +0100

    refactor: remove callbacks from BaseCredentialRequestOptions

--- packages/oid4vci/src/credential-request/types.ts
+++ packages/oid4vci/src/credential-request/types.ts
@@ -1,5 +1,3 @@
-import { CallbackContext } from "@openid4vc/oauth2";
-
 import type { CredentialRequestOptionsV1_0 } from "./v1.0";
 import type { CredentialRequestV1_0 } from "./v1.0/z-credential";
 import type { CredentialRequestOptionsV1_3 } from "./v1.3";
@@ -12,7 +10,6 @@ export type { CredentialRequestOptionsV1_0, CredentialRequestOptionsV1_3 };
  * Base options shared across all credential request versions
  */
 export interface BaseCredentialRequestOptions {
-  callbacks: Pick<CallbackContext, "signJwt">;
   clientId: string;
   credential_identifier: string;
   issuerIdentifier: string;
--- packages/oid4vci/src/credential-request/v1.0/create-credential-request.ts
+++ packages/oid4vci/src/credential-request/v1.0/create-credential-request.ts
@@ -1,4 +1,5 @@
-import { type JwtSignerJwk } from "@openid4vc/oauth2";
+import type { CallbackContext, JwtSignerJwk } from "@openid4vc/oauth2";
+
 import {
   IoWalletSdkConfig,
   ItWalletSpecsVersion,
@@ -17,6 +18,7 @@ import { CredentialRequestV1_0, zCredentialRequestV1_0 } from "./z-credential";
  */
 export interface CredentialRequestOptionsV1_0
   extends BaseCredentialRequestOptions {
+  callbacks: Pick<CallbackContext, "signJwt">;
   config: IoWalletSdkConfig<ItWalletSpecsVersion.V1_0>;
   signer: JwtSignerJwk;
   // keyAttestation is NOT accepted in v1.0
