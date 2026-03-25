f38e434fd482478d5aa1cb1b9c82a59bf954a3c1
Flavio Del Grosso <flavio.delgrosso@nttdata.com>
2026-03-25 11:37:22 +0100

    improve narrowing of jar signer method for createAuthorizationRequest

--- packages/oid4vp/src/authorization-request/create-authorization-request.ts
+++ packages/oid4vp/src/authorization-request/create-authorization-request.ts
@@ -3,6 +3,8 @@ import {
   type CreateJarRequestOptions,
   CreateJarRequestResult,
   JarAuthorizationRequest,
+  JwtSignerFederation,
+  JwtSignerX5c,
   createJarRequest,
   jwtHeaderFromJwtSigner,
   signedAuthorizationRequestJwtHeaderTyp,
@@ -10,7 +12,9 @@ import {
 import {
   IoWalletSdkConfig,
   ItWalletSpecsVersion,
+  ItWalletSpecsVersionError,
   ValidationError,
+  hasConfigVersion,
   objectToQueryParams,
   parseWithErrorHandling,
 } from "@pagopa/io-wallet-utils";
@@ -23,19 +27,23 @@ import {
   zOpenid4vpAuthorizationRequestPayload,
 } from "./z-authorization-request";
 
-type JarOptions = Pick<
+type BaseJarOptions<TSigner extends JwtSignerFederation | JwtSignerX5c> = {
+  jwtSigner: TSigner;
+} & Pick<
   CreateJarRequestOptions,
-  | "additionalJwtPayload"
-  | "expiresInSeconds"
-  | "jwtSigner"
-  | "now"
-  | "requestUri"
+  "additionalJwtPayload" | "expiresInSeconds" | "now" | "requestUri"
 >;
 
-/**
- * Options for creating an OpenID4VP authorization request URL.
- */
-export interface CreateAuthorizationRequestOptions {
+export type JarOptionsV1_0 = BaseJarOptions<JwtSignerFederation>;
+
+export type JarOptionsV1_3 = BaseJarOptions<JwtSignerX5c>;
+
+type JarOptions = JarOptionsV1_0 | JarOptionsV1_3;
+
+interface BaseCreateAuthorizationRequestOptions<
+  V extends ItWalletSpecsVersion,
+  TJar extends JarOptions,
+> {
   /**
    * Authorization request payload to be validated and serialized.
    */
@@ -47,13 +55,13 @@ export interface CreateAuthorizationRequestOptions {
   callbacks: Partial<Pick<CallbackContext, "encryptJwe">> &
     Pick<CallbackContext, "signJwt">;
 
-  config: IoWalletSdkConfig;
+  config: IoWalletSdkConfig<V>;
 
   /**
    * The request is generated as a JAR authorization request.
    * When `additionalJwtPayload.aud` is missing, it is set to `requestUri`.
    */
-  jar: JarOptions;
+  jar: TJar;
 
   /**
    * Authorization request URL scheme.
@@ -62,12 +70,41 @@ export interface CreateAuthorizationRequestOptions {
   scheme?: string;
 }
 
-export interface CreateAuthorizationRequestResult {
+/**
+ * Options for creating an OpenID4VP authorization request URL.
+ */
+export type CreateAuthorizationRequestOptionsV1_0 =
+  BaseCreateAuthorizationRequestOptions<
+    ItWalletSpecsVersion.V1_0,
+    JarOptionsV1_0
+  >;
+
+export type CreateAuthorizationRequestOptionsV1_3 =
+  BaseCreateAuthorizationRequestOptions<
+    ItWalletSpecsVersion.V1_3,
+    JarOptionsV1_3
+  >;
+
+export type CreateAuthorizationRequestOptions =
+  | CreateAuthorizationRequestOptionsV1_0
+  | CreateAuthorizationRequestOptionsV1_3;
+
+interface BaseCreateAuthorizationRequestResult<TJar extends JarOptions> {
   authorizationRequest: string;
   authorizationRequestObject: JarAuthorizationRequest;
   authorizationRequestPayload: Openid4vpAuthorizationRequestPayload;
-  jar: CreateJarRequestResult & JarOptions;
+  jar: CreateJarRequestResult & TJar;
 }
+
+export type CreateAuthorizationRequestResultV1_0 =
+  BaseCreateAuthorizationRequestResult<JarOptionsV1_0>;
+
+export type CreateAuthorizationRequestResultV1_3 =
+  BaseCreateAuthorizationRequestResult<JarOptionsV1_3>;
+
+export type CreateAuthorizationRequestResult =
+  | CreateAuthorizationRequestResultV1_0
+  | CreateAuthorizationRequestResultV1_3;
 
 /**
  * Creates an OpenID4VP authorization request URL.
@@ -81,52 +118,85 @@ export interface CreateAuthorizationRequestResult {
  * @throws When JAR creation fails
  */
 export async function createAuthorizationRequest(
+  options: CreateAuthorizationRequestOptionsV1_0,
+): Promise<CreateAuthorizationRequestResultV1_0>;
+
+export async function createAuthorizationRequest(
+  options: CreateAuthorizationRequestOptionsV1_3,
+): Promise<CreateAuthorizationRequestResultV1_3>;
+
+export async function createAuthorizationRequest(
   options: CreateAuthorizationRequestOptions,
 ): Promise<CreateAuthorizationRequestResult> {
   try {
-    const { callbacks, config, jar, scheme = "openid4vp://" } = options;
+    const { config } = options;
 
-    const headerSchema = config.isVersion(ItWalletSpecsVersion.V1_0)
-      ? zOpenid4vpAuthorizationRequestHeaderV1_0
-      : zOpenid4vpAuthorizationRequestHeaderV1_3;
+    if (hasConfigVersion(options, ItWalletSpecsVersion.V1_0)) {
+      return await createAuthorizationRequestWithHeader(
+        options,
+        zOpenid4vpAuthorizationRequestHeaderV1_0,
+      );
+    }
 
-    const authorizationRequestHeader = parseWithErrorHandling(headerSchema, {
-      ...jwtHeaderFromJwtSigner(jar.jwtSigner),
-      typ: signedAuthorizationRequestJwtHeaderTyp,
-    });
+    if (hasConfigVersion(options, ItWalletSpecsVersion.V1_3)) {
+      return await createAuthorizationRequestWithHeader(
+        options,
+        zOpenid4vpAuthorizationRequestHeaderV1_3,
+      );
+    }
 
-    const authorizationRequestPayload = parseWithErrorHandling(
-      zOpenid4vpAuthorizationRequestPayload,
-      options.authorizationRequestPayload,
+    throw new ItWalletSpecsVersionError(
+      "createAuthorizationRequest",
+      config.itWalletSpecsVersion,
+      [ItWalletSpecsVersion.V1_0, ItWalletSpecsVersion.V1_3],
     );
-
-    const additionalJwtPayload = !jar.additionalJwtPayload?.aud
-      ? { ...jar.additionalJwtPayload, aud: jar.requestUri }
-      : jar.additionalJwtPayload;
-
-    const jarResult = await createJarRequest({
-      ...jar,
-      additionalJwtPayload,
-      authorizationRequestHeader,
-      authorizationRequestPayload,
-      callbacks,
-    });
-
-    return {
-      authorizationRequest: createAuthorizationRequestUrl(
-        scheme,
-        jarResult.jarAuthorizationRequest,
-      ),
-      authorizationRequestObject: jarResult.jarAuthorizationRequest,
-      authorizationRequestPayload,
-      jar: { ...jar, ...jarResult },
-    };
   } catch (error) {
     if (error instanceof ValidationError) {
       throw new Oid4vpError(`Invalid authorization request: ${error.message}`);
     }
     throw error;
   }
+}
+
+async function createAuthorizationRequestWithHeader<TJar extends JarOptions>(
+  options: BaseCreateAuthorizationRequestOptions<ItWalletSpecsVersion, TJar>,
+  headerSchema:
+    | typeof zOpenid4vpAuthorizationRequestHeaderV1_0
+    | typeof zOpenid4vpAuthorizationRequestHeaderV1_3,
+): Promise<BaseCreateAuthorizationRequestResult<TJar>> {
+  const { callbacks, jar, scheme = "openid4vp://" } = options;
+
+  const authorizationRequestHeader = parseWithErrorHandling(headerSchema, {
+    ...jwtHeaderFromJwtSigner(jar.jwtSigner),
+    typ: signedAuthorizationRequestJwtHeaderTyp,
+  });
+
+  const authorizationRequestPayload = parseWithErrorHandling(
+    zOpenid4vpAuthorizationRequestPayload,
+    options.authorizationRequestPayload,
+  );
+
+  const additionalJwtPayload = !jar.additionalJwtPayload?.aud
+    ? { ...jar.additionalJwtPayload, aud: jar.requestUri }
+    : jar.additionalJwtPayload;
+
+  const jarResult = await createJarRequest({
+    ...jar,
+    additionalJwtPayload,
+    authorizationRequestHeader,
+    authorizationRequestPayload,
+    callbacks,
+  });
+
+  return {
+    authorizationRequest: createAuthorizationRequestUrl(
+      scheme,
+      jarResult.jarAuthorizationRequest,
+    ),
+    authorizationRequestObject: jarResult.jarAuthorizationRequest,
+    authorizationRequestPayload,
+    jar: { ...jar, ...jarResult },
+  };
 }
 
 function createAuthorizationRequestUrl(
