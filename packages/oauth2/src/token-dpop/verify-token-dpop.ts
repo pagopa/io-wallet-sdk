e88084fc975369fce67351f085aa5f3de3c0482a
Manuel Rafeli <manuel.rafeli@pagopa.it>
2026-04-03 08:57:02 +0200

    Update packages/oauth2/src/token-dpop/verify-token-dpop.ts
    Co-authored-by: sebastianopriscan <92364304+sebastianopriscan@users.noreply.github.com>

--- packages/oauth2/src/token-dpop/verify-token-dpop.ts
+++ packages/oauth2/src/token-dpop/verify-token-dpop.ts
@@ -69,7 +69,7 @@ export interface VerifyTokenDPoPOptions {
 
 export async function verifyTokenDPoP(options: VerifyTokenDPoPOptions) {
   const { header, payload } = decodeJwt({
-    errorMessagePrefix: "Error decoding DPoP JWT:",
+    errorMessagePrefix: "Error decoding access token DPoP JWT:",
     headerSchema: zDpopJwtHeader,
     jwt: options.dpopJwt,
     payloadSchema: zDpopJwtPayload,
