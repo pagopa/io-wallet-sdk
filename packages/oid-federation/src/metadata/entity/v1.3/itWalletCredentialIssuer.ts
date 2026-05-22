5486f3ac22bc9e93448c316adc4d9dd9b7cda4c6
Gianluca Spada <gianluca.spada@spindox.it>
2026-05-22 11:06:57 +0200

    fix(oid-federation): Allow numeric values in credential_signing_alg_values_supported for mdoc credentials (#159)
    This PR fixes the type of values for
    `credential_signing_alg_values_supported` of mdoc credentials, changing
    it from string to number in accordance with OpenID4VCI [Credential
    Issuance
    Metadata](https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#name-credential-issuer-metadata-5)
    for `mso_mdoc` format.

--- packages/oid-federation/src/metadata/entity/v1.3/itWalletCredentialIssuer.ts
+++ packages/oid-federation/src/metadata/entity/v1.3/itWalletCredentialIssuer.ts
@@ -113,13 +113,20 @@ export type SupportedCredentialMetadata = z.infer<
 
 export const SupportedCredentialMetadata = z.intersection(
   z.discriminatedUnion("format", [
-    z.object({ format: z.literal("dc+sd-jwt"), vct: z.string() }),
-    z.object({ doctype: z.string(), format: z.literal("mso_mdoc") }),
+    z.object({
+      credential_signing_alg_values_supported: z.array(z.string()),
+      format: z.literal("dc+sd-jwt"),
+      vct: z.string(),
+    }),
+    z.object({
+      credential_signing_alg_values_supported: z.array(z.number().int()),
+      doctype: z.string(),
+      format: z.literal("mso_mdoc"),
+    }),
   ]),
   z.object({
     authentic_sources: AuthenticSources,
     credential_metadata: CredentialMetadata,
-    credential_signing_alg_values_supported: z.array(z.string()),
     cryptographic_binding_methods_supported: z.array(z.string()),
     proof_types_supported: ProofTypesSupported,
     schema_id: z.string(),
